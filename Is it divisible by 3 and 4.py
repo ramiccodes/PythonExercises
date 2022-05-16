abaa07c11c42e96472224d0ecf2e17fde41e9098
Ramic <100771639+ramiccodes@users.noreply.github.com>
2022-05-16 12:29:01 -0700

    Update Is it divisible by 3 and 4.py

--- Is it divisible by 3 and 4.py
+++ Is it divisible by 3 and 4.py
@@ -1,14 +1,14 @@
-#enter = int(input("Enter an integer:"))
-#div_3 = 3
-#div_4 = 4
-#if enter % div_3 == 0:
-    #print("This integer is divisible by 3.")
-#else:
-    #print("This integer is not divisible by 3.")
-#if enter % div_4 == 0:
-    #print("This integer is divisible by 4.")
-#else:
-    #print("This integer is not divisible by 4.")
-#if enter % div_4 and div_3 != 0:
-    #print("This integer is not divisible by 3 and 4.")
+enter = int(input("Enter an integer:"))
+div_3 = 3
+div_4 = 4
+if enter % div_3 == 0:
+    print("This integer is divisible by 3.")
+else:
+    print("This integer is not divisible by 3.")
+if enter % div_4 == 0:
+    print("This integer i divisible by 4.")
+else:
+    print("This integer is not divisible by 4.")
+if enter % div_4 and div_3 != 0:
+    print("This integer is not divisible by 3 and 4.")
 
